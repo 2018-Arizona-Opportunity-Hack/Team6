c9fa5ba4c08db01a8be4251bd76fc7279068f07d
Pooria <pjrashidi@gmail.com>
2018-10-20 18:25:00 -0700

    added antd

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Firebase from './Firebase.js'
+import Button from 'antd/lib/button'
 import './App.css'
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <Firebase />
+        <Button type="primary">Button</Button>
       </div>
     )
   }
