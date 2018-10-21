f6f3fb6080f0599b5ecf399d6048d0e02810b5ea
yash101 <lodhad@gmail.com>
2018-10-21 01:16:42 -0700

    pushing API

--- src/api.js
+++ src/api.js
@@ -14,35 +14,72 @@
 	api.constants.NEW_USER = 1;
 	api.constants.MOD_USER = 2;
 
-	/* Data for fudging. Should not exist in the real world */
-	let users = {};
-	users["yash101"] = {
-		username: "yash101",
-		password: "password1",
-		authorization: api.constants.ADMINISTRATOR,
-		first_name: "Dev",
-		last_name: "Lodha",
-		phone_number: "+19990000000",
-		email: "hackeryash101@gmail.com"
-	};
-	users["jacob"] = {
-		username: "jacob",
-		password: "password2",
-		authorization: api.constants.MEMBER,
-		first_name: "Jake",
-		last_name: "Fl",
-		phone_number: "+19990000001",
-		email: "jake@example.com"
-	};
+	// Constants for log out
+	api.constants.LOG_OUT_SUCCESS = 0;
 
-	let internalData = {};
+
+	let internalData = undefined;
+
+	let initializeInternalData = function() {
+		internalData = {};
+		internalData.users = {};
+
+		internalData.users["yash101"] = {
+			username: "yash101",
+			password: "password1",
+			authorization: api.constants.ADMINISTRATOR,
+			first_name: "Dev",
+			last_name: "Lodha",
+			phone_number: "+19990000000",
+			email: "hackeryash101@gmail.com"
+		};
+		internalData.users["jacob"] = {
+			username: "jacob",
+			password: "password2",
+			authorization: api.constants.MEMBER,
+			first_name: "Jake",
+			last_name: "Fl",
+			phone_number: "+19990000001",
+			email: "jake@example.com"
+		};
+
+		internalData.events = [
+			{
+				event_id: null,
+				name: "Test event 0",
+				description: "A test event, event number zero! This is the description!",
+				author: "yash101",
+				from: new Date(2018, 10, 24, 12, 0, 0),
+				to: new Date(2018, 10, 25, 20, 0, 0),
+				categories: ["test"],
+				hours_needed: 10,
+				volunteers: [
+					{
+						username: "jacob",
+						hours: 5
+					}
+				]
+			}
+		];
+
+		var start = Math.floor(Math.random() * 100000);
+		for(int i = 0; i < internalData.events.length; i++) {
+			if(internalData.events[i].event_id == null) {
+				internalData.events[i].event_id = start;
+				start += 1;
+			}
+		}
+	};
+	if(!internalData) {
+		initializeInternalData();
+	}
 
 	/*
 	Returns: String when error
 	Returns: True when log in successful
 	*/
 	api.auth.logIn = function(username, password) {
-		if(users[username] && users[username].password == password) {
+		if(internalData.users[username] && internalData.users[username].password == password) {
 			internalData.currentUser = username;
 			return true;
 		}
@@ -57,7 +94,7 @@
 			return 0;
 		}
 
-		return users[internalData.currentUser].authorization;
+		return internalData.users[internalData.currentUser].authorization;
 	};
 
 	/*
@@ -71,7 +108,7 @@
 		let currentLevel = api.auth.checkLevel();
 
 		// Find the current existing user
-		let find = users[user.username];
+		let find = internalData.users[user.username];
 
 		// If the user does not exist:
 		if(!find) {
@@ -83,7 +120,7 @@
 			if(!user.phone_number) return "Phone number bad";
 			if(!user.email) return "Email bad";
 
-			users[user.username] = {
+			internalData.users[user.username] = {
 				username: user.username,
 				password: user.password,
 				first_name: user.first_name,
@@ -95,7 +132,7 @@
 
 			return api.constants.NEW_USER;
 		} else {
-			let curUser = users[user.username];
+			let curUser = internalData.users[user.username];
 
 			curUser = {
 				username: user.username || curUser.username,
@@ -111,14 +148,41 @@
 				curUser.authorization = user.authorization;
 			}
 
-			users[user.username] = curUser;
+			internalData.users[user.username] = curUser;
 
 			return api.constants.MOD_USER;
 		}
 	};
 
 	/*
+	Returns api.constants.LOG_OUT_SUCCESS log out was successful,
+	otherwise returns a string, "No user is currently logged in!"
 	*/
+	api.auth.logOut = function() {
+		if(internalData.currentUser) {
+			internalData.currentUser = undefined;
+			return api.constants.LOG_OUT_SUCCESS;
+		}
+		return "No user is currently logged in!";
+	};
+
+	api.events.getEvents(search, from, to) {
+		let s = new RegExp(search, "i");
+		let ret = [];
+		let eventsList = internalData.events;
+
+		for(var i = 0; i < eventsList.length; i++) {
+			if(eventsList[i].name.match(s) &&
+				(!from || eventsList[i].from >= from) &&
+				(!to || eventsList[i] <= eventsList[i].to)) {
+				ret.push(eventsList[i]);
+			}
+		}
+
+		return ret;
+	};
+
+	api.events.updateEvent()
 
 	window.api = api;
 })();
