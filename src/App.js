d2da0bbf74f1bf8468826bb0d5623ae68c6f8ef6
Jake Folsom <jfolsom2@asu.edu>
2018-10-20 21:27:41 -0700

    homePage is Meh

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react'
 import Firebase from './Firebase.js'
 import './App.css'
 import NavBar from './navBar.js'
+import HomePage from '../src/pages/homePage.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <NavBar/>
-          
+        <HomePage/>
       </div>
     )
   }
