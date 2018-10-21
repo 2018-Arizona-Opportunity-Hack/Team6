361517877bb179c1d56d331eb0deb2bca1955f61
Pooria <pjrashidi@gmail.com>
2018-10-20 21:35:54 -0700

    organized stuff

--- src/App.js
+++ src/App.js
@@ -3,16 +3,23 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import './App.css'
 import NavBar from './navBar.js'
 import HomePage from '../src/pages/homePage.js'
+import { Layout, Menu, Breadcrumb } from 'antd'
+const { Header, Content, Footer } = Layout
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div className="App">
-          <NavBar />
-          <Switch>
-            <Route path="/home" component={HomePage} />
-          </Switch>
+          <Layout className="layout">
+            <NavBar />
+            <Content style={{ padding: '0 50px' }}>
+              <Switch>
+                <Route path="/home" component={HomePage} />
+              </Switch>
+            </Content>
+            <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+          </Layout>
         </div>
       </BrowserRouter>
     )
--- src/navBar.js
+++ src/navBar.js
@@ -6,29 +6,23 @@ const { Header, Content, Footer } = Layout
 class navBar extends Component {
   render() {
     return (
-      <Layout className="layout">
-        <Header>
-          <div className="logo" />
-          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']} style={{ lineHeight: '64px' }}>
-            <Menu.Item key="1">
-              <Link to="/home">Home</Link>
-            </Menu.Item>
-            <Menu.Item key="2">
-              <Link to="/events">Events</Link>
-            </Menu.Item>
-            <Menu.Item key="3">
-              <Link to="/volunteers">Volunteers</Link>
-            </Menu.Item>
-            <Menu.Item key="4">
-              <Link to="/login">Login</Link>
-            </Menu.Item>
-          </Menu>
-        </Header>
-        <Content style={{ padding: '0 50px' }}>
-          <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>Content</div>
-        </Content>
-        <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
-      </Layout>
+      <Header>
+        <div className="logo" />
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']} style={{ lineHeight: '64px' }}>
+          <Menu.Item key="1">
+            <Link to="/home">Home</Link>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <Link to="/events">Events</Link>
+          </Menu.Item>
+          <Menu.Item key="3">
+            <Link to="/volunteers">Volunteers</Link>
+          </Menu.Item>
+          <Menu.Item key="4">
+            <Link to="/login">Login</Link>
+          </Menu.Item>
+        </Menu>
+      </Header>
     )
   }
 }
