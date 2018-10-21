2dea4b43dde0d6dafee8d36f6934e5851ec61421
Jake Folsom <jfolsom2@asu.edu>
2018-10-21 02:37:10 -0700

    Updated the API cause Yash' didn't

--- src/api.js
+++ src/api.js
@@ -1,123 +1,255 @@
-;(function init_api() {
-  let api = {}
-  api.constants = {}
-  api.auth = {}
-  api.events = {}
-  api.database = {}
+export default function init_api() {
+    let api = {};
+    api.constants = {};
+    api.auth = {};
+    api.events = {};
+    api.database = {};
 
-  // Constants for user level
-  api.constants.GUEST = 0
-  api.constants.MEMBER = 1
-  api.constants.ADMINISTRATOR = 2
+    // Constants for user level
+    api.constants.GUEST = 0;
+    api.constants.MEMBER = 1;
+    api.constants.ADMINISTRATOR = 2;
 
-  // Constants for user modification
-  api.constants.NEW_USER = 1
-  api.constants.MOD_USER = 2
+    // Constants for user modification
+    api.constants.NEW_USER = 1;
+    api.constants.MOD_USER = 2;
 
-  /* Data for fudging. Should not exist in the real world */
-  let users = {}
-  users['yash101'] = {
-    username: 'yash101',
-    password: 'password1',
-    authorization: api.constants.ADMINISTRATOR,
-    first_name: 'Dev',
-    last_name: 'Lodha',
-    phone_number: '+19990000000',
-    email: 'hackeryash101@gmail.com'
-  }
-  users['jacob'] = {
-    username: 'jacob',
-    password: 'password2',
-    authorization: api.constants.MEMBER,
-    first_name: 'Jake',
-    last_name: 'Fl',
-    phone_number: '+19990000001',
-    email: 'jake@example.com'
-  }
+    // Constants for log out
+    api.constants.LOG_OUT_SUCCESS = 0;
 
-  let internalData = {}
 
-  /*
-	Returns: String when error
-	Returns: True when log in successful
-	*/
-  api.auth.logIn = function(username, password) {
-    if (users[username] && users[username].password == password) {
-      internalData.currentUser = username
-      return true
+    let internalData = undefined;
+
+    api.database.loadDatabase = function() {
+        if(window.localStorage)
+            internalData = JSON.parse(window.localStorage.getItem("database"));
+    };
+
+    api.database.saveDatabase = function() {
+        if(window.localStorage)
+            window.localStorage.setItem("database", JSON.stringify(internalData));
+    };
+
+    let initializeInternalData = function() {
+        internalData = {};
+        internalData.users = {};
+
+        internalData.users["yash101"] = {
+            username: "yash101",
+            password: "password1",
+            authorization: api.constants.ADMINISTRATOR,
+            first_name: "Dev",
+            last_name: "Lodha",
+            phone_number: "+19990000000",
+            email: "hackeryash101@gmail.com"
+        };
+        internalData.users["jacob"] = {
+            username: "jacob",
+            password: "password2",
+            authorization: api.constants.MEMBER,
+            first_name: "Jake",
+            last_name: "Fl",
+            phone_number: "+19990000001",
+            email: "jake@example.com"
+        };
+
+        internalData.events = [
+            {
+                event_id: null,
+                name: "Test event 0",
+                description: "A test event, event number zero! This is the description!",
+                author: "yash101",
+                from: new Date(2018, 10, 24, 12, 0, 0),
+                to: new Date(2018, 10, 25, 20, 0, 0),
+                categories: ["test"],
+                hours_needed: 10,
+                volunteers: [
+                    {
+                        username: "jacob",
+                        hours: 5
+                    }
+                ]
+            },
+            {
+                event_id: null,
+                name: "TestTest123",
+                description: "A test event, event number zero! This is the description!",
+                author: "yash101",
+                from: new Date(2018, 10, 24, 12, 0, 0),
+                to: new Date(2018, 10, 25, 20, 0, 0),
+                categories: ["test"],
+                hours_needed: 10,
+                volunteers: [
+                    {
+                        username: "jacob",
+                        hours: 5
+                    }
+                ]
+            },
+            {
+                event_id: null,
+                name: "Event 1",
+                description: "A test event, event number zero! This is the description!",
+                author: "yash101",
+                from: new Date(2018, 10, 24, 12, 0, 0),
+                to: new Date(2018, 10, 25, 20, 0, 0),
+                categories: ["test"],
+                hours_needed: 10,
+                volunteers: [
+                    {
+                        username: "jacob",
+                        hours: 5
+                    }
+                ]
+            }
+        ];
+
+        var start = Math.floor(Math.random() * 100000);
+        for(var i = 0; i < internalData.events.length; i++) {
+            if(internalData.events[i].event_id == null) {
+                internalData.events[i].event_id = start;
+                start += 1;
+            }
+        }
+    };
+
+    api.database.loadDatabase();
+
+    if(!internalData) {
+        initializeInternalData();
+        api.database.saveDatabase();
     }
-    return 'Invalid Username or Password'
-  }
 
-  /*
-	Returns: int authorization level. 0 if undefined (guest)
-	*/
-  api.auth.checkLevel = function() {
-    if (!internalData.currentUser) {
-      return 0
-    }
+    /*
+    Returns: String when error
+    Returns: True when log in successful
+    */
+    api.auth.logIn = function(username, password) {
+        if(internalData.users[username] && internalData.users[username].password === password) {
+            internalData.currentUser = username;
+            api.database.saveDatabase();
+            return true;
+        }
+        return "Invalid Username or Password";
+    };
 
-    return users[internalData.currentUser].authorization
-  }
+    /*
+    Returns: int authorization level. 0 if undefined (guest)
+    */
+    api.auth.checkLevel = function() {
+        if(!internalData.currentUser) {
+            return 0;
+        }
 
-  /*
-	returns api.constants.MOD_USER or api.constants.NEW_USER if successful
-	returns a string with error if failure
-	*/
-  api.auth.modifyUser = function(user) {
-    // user = {username, password, authorization, first_name, last_name, phone_number, email}
+        return internalData.users[internalData.currentUser].authorization;
+    };
 
-    let currentLevel = api.auth.checkLevel()
+    /*
+    returns api.constants.MOD_USER or api.constants.NEW_USER if successful
+    returns a string with error if failure
+    */
+    api.auth.modifyUser = function(user) {
 
-    // Find the current existing user
-    let find = users[user.username]
+        // user = {username, password, authorization, first_name, last_name, phone_number, email}
 
-    // If the user does not exist:
-    if (!find) {
-      // Create new user
-      if (!user.username) return 'Username bad';
-      if (!user.password) return 'Password bad';
-      if (!user.first_name) return 'First name bad';
-      if (!user.last_name) return 'Last name bad';
-      if (!user.phone_number) return 'Phone number bad';
-      if (!user.email) return 'Email bad';
+        let currentLevel = api.auth.checkLevel();
 
-      users[user.username] = {
-        username: user.username,
-        password: user.password,
-        first_name: user.first_name,
-        last_name: user.last_name,
-        phone_number: user.phone_number,
-        email: user.email,
-        authorization: 1
-      };
+        // Find the current existing user
+        let find = internalData.users[user.username];
 
-      return api.constants.NEW_USER
-    } else {
-      let curUser = users[user.username]
+        // If the user does not exist:
+        if(!find) {
+            // Create new user
+            if(!user.username) return "Username bad";
+            if(!user.password) return "Password bad";
+            if(!user.first_name) return "First name bad";
+            if(!user.last_name) return "Last name bad";
+            if(!user.phone_number) return "Phone number bad";
+            if(!user.email) return "Email bad";
 
-      curUser = {
-        username: user.username || curUser.username,
-        password: user.password || curUser.password,
-        first_name: user.first_name || curUser.first_name,
-        last_name: user.last_name || curUser.last_name,
-        phone_number: user.phone_number || curUser.phone_number,
-        email: user.email || curUser.email,
-        authorization: curUser.authorization
-      };
+            internalData.users[user.username] = {
+                username: user.username,
+                password: user.password,
+                first_name: user.first_name,
+                last_name: user.last_name,
+                phone_number: user.phone_number,
+                email: user.email,
+                authorization: 1
+            };
 
-      if (currentLevel >= api.constants.ADMINISTRATOR && user.authorization != undefined) {
-        curUser.authorization = user.authorization
-      }
+            api.database.saveDatabase();
+            return api.constants.NEW_USER;
+        } else {
+            let curUser = internalData.users[user.username];
 
-      users[user.username] = curUser
+            if(user.remove === true)
+                delete internalData.users[user.username];
 
-      return api.constants.MOD_USER
-    }
-  };
+            curUser = {
+                username: user.username || curUser.username,
+                password: user.password || curUser.password,
+                first_name: user.first_name || curUser.first_name,
+                last_name: user.last_name || curUser.last_name,
+                phone_number: user.phone_number || curUser.phone_number,
+                email: user.email || curUser.email,
+                authorization: curUser.authorization
+            };
 
-  /*
-	*/
+            if(currentLevel >= api.constants.ADMINISTRATOR && user.authorization !== undefined) {
+                curUser.authorization = user.authorization;
+            } else if(currentLevel < api.constants.ADMINISTRATOR) {
+                return "Error: permission to change user permission denied!";
+            }
 
-  window.api = api
-})()
+            internalData.users[user.username] = curUser;
+
+            api.database.saveDatabase();
+            return api.constants.MOD_USER;
+        }
+    };
+
+    /*
+    Returns api.constants.LOG_OUT_SUCCESS log out was successful,
+    otherwise returns a string, "No user is currently logged in!"
+    */
+    api.auth.logOut = function() {
+        if(internalData.currentUser) {
+            internalData.currentUser = undefined;
+            api.database.saveDatabase();
+            return api.constants.LOG_OUT_SUCCESS;
+        }
+        return "No user is currently logged in!";
+    };
+
+    api.events.getEvents = function(search, from, to) {
+        let ret = [];
+        let eventsList = internalData.events;
+
+        for(var i = 0; i < eventsList.length; i++) {
+            let s = new RegExp(search, "i");
+
+            if(eventsList[i].name.match(s) &&
+                (!from || eventsList[i].from >= from) &&
+                (!to || eventsList[i] <= eventsList[i].to)) {
+                ret.push(eventsList[i]);
+            }
+
+        }
+
+        return ret;
+    };
+
+    api.events.updateEvent = function(eid) {
+        api.database.saveDatabase();
+    };
+
+    api.database.importCSV = function(csv_data) {
+    };
+
+    api.database.exportCSV = function() {
+    };
+
+    window.api = api;
+    api.debug = {};
+    api.debug.data = internalData;
+};
