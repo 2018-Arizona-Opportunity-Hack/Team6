1f3b35f0e07ab01756da262eefe6a11b2bde4115
Jake Folsom <jfolsom2@asu.edu>
2018-10-21 02:05:23 -0700

    VolunteerPAge

---
+++ src/components/volunteer.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import { Icon, Card } from 'antd'
+import { Form, Input, Checkbox, Button, AutoComplete } from 'antd';
+
+class event extends Component {
+    state={
+        firstName: '',
+        lastName: '',
+        phone: '',
+        email: ''
+    }
+
+    render() {
+        let { id, events, email, phone, firstName, lastName, categories}= this.props;
+        /*
+        if (this.state.editmode)
+        {
+            then do the same thing but with tex
+        }
+        */
+
+        return(
+            <Card>
+                <p>{firstName + ' ' + lastName}</p>
+                <Input type="text" value={firstName} prefix={<Icon type ="user" theme="outlined"/>}/>
+                <Input type="text" value={lastName} prefix={<Icon type="user"theme="outlined"/>}/>
+                <Input type="text" value={phone}prefix={<Icon type ="phone" theme="outlined"/>}/>
+                <Input type="text" value={email}prefix={<Icon type="mail" theme="outlined"/>}/>
+                <Button type="primary" icon="check">Save</Button>
+            </Card>
+         )
+
+        return (
+            <Card title= "Volunteer">
+                {/* replace with username */}
+                <p>{firstName + ' ' + lastName}</p>
+                <p>
+                    <Icon type="user" theme="outlined" /> {firstName} {lastName}
+                </p>
+                <p>
+                    <Icon type="phone" theme="outlined" /> {phone} Phone #
+                </p>
+                <p>
+                    <Icon type="mail" theme="outlined" /> {email} Email
+                </p>
+                <Button type="primary" icon="edit">Edit</Button>
+            </Card>
+        )
+    }
+}
+
+export default event
--- src/pages/volunteerPage.js
+++ src/pages/volunteerPage.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
 import Volunteer from '../components/volunteer.js'
+import {Col, Row} from "antd";
+import { Card } from "antd";
+const gridStyle = {
+    width: '25%',
+    textAlign: 'center',
+};
 
-let volunteerObject = {
+let volunteerObj = {
     id:1,
-    event: [],
+    events: [],
     email: 'www.stuff.com',
     phone: '623-612-2349',
     f_name: 'Bob',
@@ -14,12 +20,44 @@ let volunteerObject = {
 class volunteerPage extends Component {
   render() {
     return (
-      <div>
-        <h1>Volunteer page goes here</h1>
-        <Volunteer id={volunteerObject.id} event={volunteerObject.event} email = {volunteerObject.email}
-                   phone = {volunteerObject.phone} f_name={volunteerObject.f_name}
-                   l_name = {volunteerObject.l_name} categories = {volunteerObject.categories}/>
-      </div>
+        <div>
+            <h1>Volunteers go here</h1>
+            <Col gutter={16}>
+                <Row span={8}>
+                    <Volunteer
+                        id={volunteerObj.id}
+                        events={volunteerObj.events}
+                        email={volunteerObj.email}
+                        phone={volunteerObj.phone}
+                        firstName={volunteerObj.f_name}
+                        lastName={volunteerObj.l_name}
+                        categories={volunteerObj.categories}
+                    />
+                </Row>
+                <Row span={8}>
+                    <Volunteer
+                        id={volunteerObj.id + ' 2'}
+                        events={volunteerObj.events + ' 2'}
+                        email={volunteerObj.email + ' 2'}
+                        phone={volunteerObj.phone + ' 2'}
+                        firstName={volunteerObj.f_name+ ' 2'}
+                        lastName={volunteerObj.l_name + ' 2'}
+                        categories={volunteerObj.categories + ' 2'}
+                    />
+                </Row>
+                <Row span={8}>
+                    <Volunteer
+                        id={volunteerObj.id + ' 3'}
+                        events={volunteerObj.events + ' 3'}
+                        email={volunteerObj.email + ' 3'}
+                        phone={volunteerObj.phone + ' 3'}
+                        firstName={volunteerObj.f_name + ' 3'}
+                        lastName={volunteerObj.l_name + ' 3'}
+                        categories={volunteerObj.categories + ' 3'}
+                    />
+                </Row>
+            </Col>
+        </div>
     )
   }
 }
