58f1f3ee5625399f213e596d044afc93064fad71
Pooria <pjrashidi@gmail.com>
2018-10-20 18:36:23 -0700

    removed test ant components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import Firebase from './Firebase.js'
-import Button from 'antd/lib/button'
 import './App.css'
 
 class App extends Component {
@@ -8,7 +7,6 @@ class App extends Component {
     return (
       <div className="App">
         <Firebase />
-        <Button type="primary">Button</Button>
       </div>
     )
   }
